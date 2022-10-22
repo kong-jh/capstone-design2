baf8fc8cebe25dcb60020fadcf0ccce221b51500
Jounghun Kong <kjhun1208@khu.ac.kr>
2022-10-22 13:49:49 +0900

    Add bike rental location

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
 var express = require('express');
 var app = express();
 
+// env file
 require('dotenv').config();
 
+// MVC - ejs
 app.set('view engine', 'ejs');
 app.set('views', 'views'); 
+
+//mysql
+var mysql = require('mysql');
+const password = process.env.DATABASE_PASSWORD;
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'nodeuser',
+    password: password,
+    database: 'mobility',
+    port : 3306
+});
+connection.connect();
+
 
 app.get('/', function(req, res) {
     res.render('view', {
         appkey : process.env.appkey
     });
 })
+
+app.get('/bike', (req, res) => {
+    var sql = 'select id, name, lat, lon from bike3';
+    connection.query(sql, (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('error');
+      }
+      res.render('bike_view', {appkey : process.env.appkey, results:results});
+    });
+  });
 
 var server = app.listen(3000, function () {
     var host = server.address().address;
